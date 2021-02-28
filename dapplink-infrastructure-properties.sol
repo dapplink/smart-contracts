5f22c3d24622c22d7e241ea3f066f2c2a39663a0
Dapplink <67794448+dapplink@users.noreply.github.com>
2021-02-28 04:49:47 +0200

    Add DapplinkInfrastructureProperties contract

---
+++ dapplink-infrastructure-properties.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.5.0;
+
+
+contract DapplinkInfrastructureProperties {
+
+
+    string public symbol;
+    string public name;
+    uint8  public decimals;
+    uint   public _totalSupply;
+    uint   public price;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+
+}
